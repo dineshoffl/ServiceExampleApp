8f5bcaa2e5facc4ae179e8e6a591fb15e7f44ce4
DineshKumar <dinesh@apna.co>
2020-11-02 21:04:10 +0530

    BroadCast receiver removed

--- app/src/main/java/com/example/serviceexampleapp/MainActivity.kt
+++ app/src/main/java/com/example/serviceexampleapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.serviceexampleapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 
@@ -13,6 +14,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val serviceIntent = Intent(this, ForeGroundService::class.java)
         ContextCompat.startForegroundService(this, serviceIntent)
+        if (intent?.getBooleanExtra("isButtonClicked", false) == true) {
+            Log.d("BtnClicked", "Click done")
+        }
 
     }
+
 }
