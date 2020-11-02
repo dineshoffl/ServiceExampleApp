8e345b984264870c701a9332665fe1b05f0dd7d1
DineshKumar <dinesh@apna.co>
2020-11-02 15:52:30 +0530

    ForeGroundService added

--- app/src/main/java/com/example/serviceexampleapp/MainActivity.kt
+++ app/src/main/java/com/example/serviceexampleapp/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.serviceexampleapp
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Utils.scheduleJobService(this)
+        val serviceIntent = Intent(this, ForeGroundService::class.java)
+        ContextCompat.startForegroundService(this, serviceIntent)
 
     }
 }
